4458cd976e24ab08ff730cf2b44573641115fdd9
DxsGeorge <georgepetrg@gmail.com>
2014-02-04 03:07:59 +0200

    nothing

--- physics.cpp
+++ physics.cpp
@@ -37,3 +37,12 @@ void checkMolCollision(Molecule3& m1, Molecule3& m2, float e){
 	if (m1.cm.distance(m2.cm)<m1.R+m2.R){
 	}
 }
+
+void MolCollisionResponse(Molecule3& m1, Molecule3& m2, float e){
+};
+
+void MolToWallCheck(Molecule3& m){
+};
+
+void UpdateMolPos(Molecule3& m, float dt){
+};
